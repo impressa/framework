57ebde8da29673939c6a3e924f5783199ed819bf
puty <tputnoky@gmail.com>
2012-09-23 19:03:01 +0200

    composer

--- Impressa/Config/Configurator.php
+++ Impressa/Config/Configurator.php
@@ -27,40 +27,40 @@ class Configurator extends \Nette\Config\Configurator
         return $compiler;
     }
 
-	public function createContainer() {
-		$container = parent::createContainer();
-		$cacheDir = $this->getCacheDirectory();
-		$cache = new \Nette\Caching\Cache(new \Nette\Caching\Storages\PhpFileStorage($cacheDir), 'Impressa.Accessors');
-		$cacheKey = array($this->parameters, $this->files);
-
-		$cached = $cache->load($cacheKey);
-		if(!$cached){
-			$code = $this->buildAccessors($container);
-			$cache->save($cacheKey, $code);
-			$cached = $cache->load($cacheKey);
-		}
-		\Nette\Utils\LimitedScope::load($cached['file'], TRUE);
-		return $container;
-
-	}
-
-	protected function buildAccessors($container){
-		$code = "<?php\n";
-		foreach ($container->getReflection()->getMethods() as $method) {
-			/* @var $method  \Nette\Reflection\Method*/
-			if(\Nette\Utils\Strings::startsWith($method->name, 'createService')){
-				$type = $method->getAnnotation('return');
-				$serviceName = substr($method->name,strlen('createService'));
-				$interface = new \Nette\Utils\PhpGenerator\ClassType($serviceName . 'Accessor');
-				$interface->type = 'interface';
-				$interface->addMethod('get')->addDocument("@return $type");
-				$code .= $interface . "\n";
-			}
-
-		}
-
-		return $code;
-
-	}
+//	public function createContainer() {
+//		$container = parent::createContainer();
+//		$cacheDir = $this->getCacheDirectory();
+//		$cache = new \Nette\Caching\Cache(new \Nette\Caching\Storages\PhpFileStorage($cacheDir), 'Impressa.Accessors');
+//		$cacheKey = array($this->parameters, $this->files);
+//
+//		$cached = $cache->load($cacheKey);
+//		if(!$cached){
+//			$code = $this->buildAccessors($container);
+//			$cache->save($cacheKey, $code);
+//			$cached = $cache->load($cacheKey);
+//		}
+//		\Nette\Utils\LimitedScope::load($cached['file'], TRUE);
+//		return $container;
+//
+//	}
+//
+//	protected function buildAccessors($container){
+//		$code = "<?php\n";
+//		foreach ($container->getReflection()->getMethods() as $method) {
+//			/* @var $method  \Nette\Reflection\Method*/
+//			if(\Nette\Utils\Strings::startsWith($method->name, 'createService')){
+//				$type = $method->getAnnotation('return');
+//				$serviceName = substr($method->name,strlen('createService'));
+//				$interface = new \Nette\Utils\PhpGenerator\ClassType($serviceName . 'Accessor');
+//				$interface->type = 'interface';
+//				$interface->addMethod('get')->addDocument("@return $type");
+//				$code .= $interface . "\n";
+//			}
+//
+//		}
+//
+//		return $code;
+//
+//	}
 
 }
