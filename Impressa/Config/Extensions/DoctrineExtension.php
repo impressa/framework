81679efb2e1a246799016292237130f07b69758e
puty <tputnoky@gmail.com>
2012-11-08 17:15:45 +0100

    extension

--- Impressa/Config/Extensions/DoctrineExtension.php
+++ Impressa/Config/Extensions/DoctrineExtension.php
@@ -11,9 +11,9 @@
 namespace Impressa\Config\Extensions;
 
 use Nette\Config\Configurator,
-Nette\DI\ContainerBuilder,
-Doctrine\Common\Cache\Cache,
-Nette\Framework;
+    Nette\DI\ContainerBuilder,
+    Doctrine\Common\Cache\Cache,
+    Nette\Framework;
 
 /**
  * Doctrine Nella Framework services.
@@ -52,36 +52,48 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
             ->setClass('Impressa\Doctrine\Cache', array('@cacheStorage'));
 
         // metadata driver
-        $container->addDefinition($this->prefix('metadataDriver'))
-            ->setClass('Doctrine\ORM\Mapping\Driver\AnnotationDriver')
-            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createYamlMetadataDriver', array(
-            '%database.mappings%'
-        ));
+//        $container->addDefinition($this->prefix('metadataDriver'))
+//            ->setClass('Doctrine\ORM\Mapping\Driver\AnnotationDriver')
+//            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createYamlMetadataDriver', array(
+//            '%database.mappings%'
+//        ));
 
         // logger
         $container->addDefinition($this->prefix('logger'))
             ->setClass('Doctrine\DBAL\Logging\SQLLogger')
             ->setFactory('Impressa\Doctrine\Panel::register');
 
-        // configuration
-        $container->addDefinition($this->prefix('configuration'))
-            ->setClass('Doctrine\ORM\Configuration')
-            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createConfiguration', array(
-             $this->prefix('@cache'), $this->prefix('@cache'),
-            '%database%', $this->prefix('@logger'), '%productionMode%'
+        $container->addDefinition($this->prefix('annotationReader'))
+            ->setClass('\Doctrine\Common\Annotations\Reader')
+            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createAnnotationReader');
+
+
+        $container->addDefinition($this->prefix('metadataDriver'))
+            ->setClass('Doctrine\Common\Persistence\Mapping\Driver\MappingDriver')
+            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createMetadataDriver', array(
+            $this->prefix('@annotationReader'), '%database%'
         ));
+
 
         // event manager
         $container->addDefinition($this->prefix('eventManager'))
             ->setClass('Doctrine\Common\EventManager')
             ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createEventManager');
 
+        // configuration
+        $container->addDefinition($this->prefix('configuration'))
+            ->setClass('Doctrine\ORM\Configuration')
+            ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createConfiguration', array(
+            $this->prefix('@metadataDriver'),$this->prefix('@cache'), $this->prefix('@cache'),
+            '%database%', $this->prefix('@logger'), '%productionMode%'
+        ));
+
         // connection factory
         $connectionFactory = $container->addDefinition($this->prefix('newConnection'))
             ->setClass('Doctrine\DBAL\Connection')
             ->setParameters(array('config', 'configuration', 'eventManager' => NULL))
             ->setFactory('Impressa\Config\Extensions\DoctrineExtension::createConnection', array(
-            '%config%', '%configuration%', '%eventManager%'
+            '%config%', '%configuration%', '%eventManager%', $this->prefix('@annotationReader')
         ))
             ->setShared(FALSE);
 
@@ -109,32 +121,32 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
             ->setAutowired(FALSE);
 
         //$drv = self::createYamlMetadataDriver(\Nette\DI\Helpers::expand('%database.mappings%', $container->parameters, true));
-        $conf = new \Doctrine\ORM\Configuration;
-        $drv = $conf->newDefaultAnnotationDriver(\Nette\DI\Helpers::expand('%database.entityDirs%', $container->parameters, TRUE));
-        $conf->setMetadataDriverImpl($drv);
-        $conf->setProxyDir(\Nette\DI\Helpers::expand('%database.proxyDir%', $container->parameters, TRUE));
-        $conf->setProxyNamespace(\Nette\DI\Helpers::expand('%database.proxyNamespace%', $container->parameters, TRUE));
-
-        $em = \Doctrine\ORM\EntityManager::create(self::createConnection($container->parameters['database'], $conf), $conf);
-
-
-
-
-        foreach ($drv->getAllClassNames() as $className) {
-            try {
-                $meta = $em->getClassMetadata($className);
-
-                $repositoryClassName = '\\Repositories\\' . $meta->getReflectionClass()->getShortName() . 'Repository';
-                $repo = \Nette\Loaders\RobotLoader::load($repositoryClassName);
-                $container->addDefinition($this->prefix(lcfirst($meta->getReflectionClass()->getShortName() . 'Repository')))
-                    ->setClass($repositoryClassName)
-                    ->setFactory("Impressa\Config\Extensions\DoctrineExtension::createRepository", array($this->prefix('@entityManager'), $className));
-            } catch (\ReflectionException $e) {
-                //TODO: zalogovat ze nie je vygenerovana entita
-            } catch (\Nette\InvalidStateException $e) {
-                //TODO: zalogovat, ze neexistuje repozitar
-            }
-        }
+//        $conf = new \Doctrine\ORM\Configuration;
+//        $drv = $conf->newDefaultAnnotationDriver(\Nette\DI\Helpers::expand('%database.entityDirs%', $container->parameters, TRUE));
+//        $conf->setMetadataDriverImpl($drv);
+//        $conf->setProxyDir(\Nette\DI\Helpers::expand('%database.proxyDir%', $container->parameters, TRUE));
+//        $conf->setProxyNamespace(\Nette\DI\Helpers::expand('%database.proxyNamespace%', $container->parameters, TRUE));
+//
+//        $em = \Doctrine\ORM\EntityManager::create(self::createConnection($container->parameters['database'], $conf), $conf);
+//
+//
+//
+//
+//        foreach ($drv->getAllClassNames() as $className) {
+//            try {
+//                $meta = $em->getClassMetadata($className);
+//
+//                $repositoryClassName = '\\Repositories\\' . $meta->getReflectionClass()->getShortName() . 'Repository';
+//                $repo = \Nette\Loaders\RobotLoader::load($repositoryClassName);
+//                $container->addDefinition($this->prefix(lcfirst($meta->getReflectionClass()->getShortName() . 'Repository')))
+//                    ->setClass($repositoryClassName)
+//                    ->setFactory("Impressa\Config\Extensions\DoctrineExtension::createRepository", array($this->prefix('@entityManager'), $className));
+//            } catch (\ReflectionException $e) {
+//                //TODO: zalogovat ze nie je vygenerovana entita
+//            } catch (\Nette\InvalidStateException $e) {
+//                //TODO: zalogovat, ze neexistuje repozitar
+//            }
+//        }
 
 
         // aliases
@@ -146,25 +158,25 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
 //			->setFactory('@container::getService', array($this->prefix('console')));
 
 
-		$container->addDefinition('facadeFactory')
-			->setClass('Impressa\Model\FacadeFactory');
+        $container->addDefinition('facadeFactory')
+            ->setClass('Impressa\Model\FacadeFactory');
 
 
-		foreach ($this->config['facades'] as $name => $class) {
-			$container->addDefinition($name)
-				->setFactory('@facadeFactory::createFacade', array($class, $this->prefix('@entityManager')))
-				->setClass($class);
+        foreach ($this->config['facades'] as $name => $class) {
+            $container->addDefinition($name)
+                ->setFactory('@facadeFactory::createFacade', array($class, $this->prefix('@entityManager')))
+                ->setClass($class);
 
 
-		}
+        }
 
     }
 
-	public static function getSomething(){
+    public static function getSomething(){
 
-	}
+    }
 
-	/**
+    /**
      * @param \Nette\DI\ContainerBuilder
      */
     protected function initDefaultParameters(ContainerBuilder $container)
@@ -203,6 +215,42 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
         return new \Doctrine\ORM\Mapping\Driver\YamlDriver($paths);
     }
 
+    public static function createAnnotationReader(){
+        $cache = new \Doctrine\Common\Cache\ArrayCache();
+// standard annotation reader
+        $annotationReader = new \Doctrine\Common\Annotations\AnnotationReader();
+
+        $cachedAnnotationReader = new \Doctrine\Common\Annotations\CachedReader(
+            $annotationReader, // use reader
+            $cache // and a cache driver
+        );
+        return $cachedAnnotationReader;
+
+    }
+
+    public static function createMetadataDriver(\Doctrine\Common\Annotations\Reader $annotationReader, array $config){
+        // Metadata
+
+        $driverChain = new \Doctrine\ORM\Mapping\Driver\DriverChain();
+// load superclass metadata mapping only, into driver chain
+// also registers Gedmo annotations.NOTE: you can personalize it
+        \Gedmo\DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
+            $driverChain, // our metadata driver chain, to hook into
+            $annotationReader // our cached annotation reader
+        );
+
+// now we want to register our application entities,
+// for that we need another metadata driver used for Entity namespace
+        $annotationDriver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(
+            $annotationReader, // our cached annotation reader
+            $config['entityDirs'] // paths to look in
+        );
+// NOTE: driver for application Entity can be different, Yaml, Xml or whatever
+// register annotation driver for our application Entity namespace
+        $driverChain->addDriver($annotationDriver, 'Entities');
+
+        return $driverChain;
+    }
 
 
     /**
@@ -214,16 +262,20 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
      * @param bool
      * @return \Doctrine\ORM\Configuration
      */
-    public static function createConfiguration(Cache $metadataCache, Cache $queryCache, array $config, \Doctrine\DBAL\Logging\SQLLogger $logger = NULL, $productionMode = FALSE)
+    public static function createConfiguration(\Doctrine\Common\Persistence\Mapping\Driver\MappingDriver $metadataDriver, $metadataCache, Cache $queryCache, array $config, \Doctrine\DBAL\Logging\SQLLogger $logger = NULL, $productionMode = FALSE)
     {
         $configuration = new \Doctrine\ORM\Configuration;
+
+        \Doctrine\Common\Annotations\AnnotationRegistry::registerFile(
+            LIBS_DIR .'/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php'
+        );
 
         // Cache
         $configuration->setMetadataCacheImpl($metadataCache);
         $configuration->setQueryCacheImpl($queryCache);
 
-        // Metadata
-        $metadataDriver = $configuration->newDefaultAnnotationDriver($config['entityDirs']);
+
+
         $configuration->setMetadataDriverImpl($metadataDriver);
 
         // Proxies
@@ -266,7 +318,7 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
      * @param \Doctrine\Common\EventManager|NULL
      * @return \Doctrine\DBAL\Connection
      */
-    public static function createConnection(array $config, \Doctrine\ORM\Configuration $cfg, \Doctrine\Common\EventManager $evm = NULL)
+    public static function createConnection(array $config, \Doctrine\ORM\Configuration $cfg, \Doctrine\Common\EventManager $evm = NULL, \Doctrine\Common\Annotations\Reader $annotationsReader)
     {
         if (!$evm) {
             $evm = new \Doctrine\Common\EventManager;
@@ -277,6 +329,15 @@ class DoctrineExtension extends \Nette\Config\CompilerExtension
                 new \Doctrine\DBAL\Event\Listeners\MysqlSessionInit($config['charset'])
             );
         }
+
+        // translatable
+        $translatableListener = new \Gedmo\Translatable\TranslatableListener;
+// current translation locale should be set from session or hook later into the listener
+// most important, before entity manager is flushed
+        $translatableListener->setTranslatableLocale('en');
+        $translatableListener->setDefaultLocale('en');
+        $translatableListener->setAnnotationReader($annotationsReader);
+        $evm->addEventSubscriber($translatableListener);
 
         return \Doctrine\DBAL\DriverManager::getConnection($config, $cfg, $evm);
     }
