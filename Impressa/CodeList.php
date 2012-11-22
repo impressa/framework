f623b7134b5d7a41e22c4191a61b6d6600d199ee
rasto <rastislav.kudla@impressa.sk>
2012-11-22 13:12:59 +0100

    DUMMY

--- Impressa/CodeList.php
+++ Impressa/CodeList.php
@@ -4,22 +4,24 @@
  * and open the template in the editor.
  */
 namespace Impressa;
+
 /**
  * Description of CodeList
  *
  * @author puty
  */
-abstract class CodeList {
+abstract class CodeList
+{
 
-    /**
-     *
-     * @param string $code
-     */
-    public static function getName($code){
-        if(is_null($code)){
-            return null;
-        }
-        $codes = static::getCodes();
-        return $codes[$code];
-    }
+	/**
+	 *
+	 * @param string $code
+	 */
+	public static function getName($code) {
+		if (is_null($code)) {
+			return NULL;
+		}
+		$codes = static::getCodes();
+		return $codes[$code];
+	}
 }    
