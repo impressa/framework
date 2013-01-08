e1b3d4ccc2834e5fbe129704489ae4ddaf915850
puty <tputnoky@gmail.com>
2013-01-08 14:38:43 +0100

    ws presenter

---
+++ Impressa/Application/WebServicePresenter.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: puty
+ * Date: 7.1.2013
+ * Time: 14:41
+ * To change this template use File | Settings | File Templates.
+ */
+class WebServicePresenter
+{
+
+}
