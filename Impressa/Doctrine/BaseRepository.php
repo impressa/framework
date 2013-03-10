20d23c560caee284a8c7dd63be23b66cd9bffdf3
rasto <rastislav.kudla@impressa.sk>
2013-03-10 17:58:33 +0100

    FIX: v pripade, ze je v DB prazdny string namiesto NULL, tak dalsie prazde stringy sa mu nezdali unikatne napr. nevyplnene ICO sposobovalo error, ze nie je unikatne

--- Impressa/Doctrine/BaseRepository.php
+++ Impressa/Doctrine/BaseRepository.php
@@ -9,20 +9,28 @@
 namespace Impressa\Doctrine;
 class BaseRepository extends \Doctrine\ORM\EntityRepository
 {
-    /**
-     * @param $paginator \Nette\Utils\Paginator
-     * @param $qb \Doctrine\ORM\QueryBuilder
-     * @return array
-     */
-    protected function getPaginatedResults(\Nette\Utils\Paginator $paginator, \Doctrine\ORM\QueryBuilder $qb){
-        $qb->setFirstResult($paginator->offset);
-        $qb->setMaxResults($paginator->itemsPerPage);
-        $results = new \Doctrine\ORM\Tools\Pagination\Paginator($qb->getQuery(), true);
-        $paginator->setItemCount(count($results));
-        return new \Impressa\Doctrine\PaginatedResult($results);
-    }
+	/**
+	 * @param $paginator \Nette\Utils\Paginator
+	 * @param $qb \Doctrine\ORM\QueryBuilder
+	 * @return array
+	 */
+	protected function getPaginatedResults(\Nette\Utils\Paginator $paginator, \Doctrine\ORM\QueryBuilder $qb)
+	{
+		$qb->setFirstResult($paginator->offset);
+		$qb->setMaxResults($paginator->itemsPerPage);
+		$results = new \Doctrine\ORM\Tools\Pagination\Paginator($qb->getQuery(), TRUE);
+		$paginator->setItemCount(count($results));
+		return new \Impressa\Doctrine\PaginatedResult($results);
+	}
 
-	public function checkUniqueValue($fieldName, $fieldValue, Entity $entity = null){
+	public function checkUniqueValue($fieldName, $fieldValue, Entity $entity = NULL)
+	{
+		// FIX: v pripade, ze je v DB prazdny string namiesto NULL, tak dalsie prazde stringy sa mu nezdali unikatne
+		// napr. nevyplnene ICO sposobovalo error, ze nie je unikatne
+		if ($fieldValue == '') {
+			return TRUE;
+		}
+
 		$existingEntity = $this->findOneBy(array($fieldName => $fieldValue));
 		return !($existingEntity && $existingEntity != $entity);
 	}
