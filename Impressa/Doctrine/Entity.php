0db41343de86f68a8c222b57dec5d05dc6c99566
rasto <rastislav.kudla@impressa.sk>
2014-06-30 16:35:25 +0200

    nastavenie exclude na serialiaciu fieldu SCALARS

--- Impressa/Doctrine/Entity.php
+++ Impressa/Doctrine/Entity.php
@@ -5,6 +5,9 @@
  * and open the template in the editor.
  */
 namespace Impressa\Doctrine;
+
+use JMS\Serializer\Annotation as JMS;
+
 /**
  * Description of Entity
  *
@@ -12,24 +15,27 @@ namespace Impressa\Doctrine;
  */
 class Entity extends \Nette\Object implements \ArrayAccess{
 
-    public $scalars = array();
+	/**
+	 * @JMS\Exclude
+	 */
+	public $scalars = array();
 
-    public function offsetExists($offset) {
-        // In this example we say that exists means it is not null
-        $value = $this->{"get$offset"}();
-        return $value !== null;
-    }
+	public function offsetExists($offset) {
+		// In this example we say that exists means it is not null
+		$value = $this->{"get$offset"}();
+		return $value !== null;
+	}
 
-    public function offsetSet($offset, $value) {
-        $this->{"set$offset"}($value);
-    }
+	public function offsetSet($offset, $value) {
+		$this->{"set$offset"}($value);
+	}
 
-    public function offsetGet($offset) {
-        return $this->{"get$offset"}();
-    }
+	public function offsetGet($offset) {
+		return $this->{"get$offset"}();
+	}
 
-    public function offsetUnset($offset) {
-         throw new \BadMethodCallException("Array access of class " . get_class($this) . " does not support unset!");
-    
-    }
+	public function offsetUnset($offset) {
+		throw new \BadMethodCallException("Array access of class " . get_class($this) . " does not support unset!");
+
+	}
 }
